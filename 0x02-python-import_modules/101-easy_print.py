119179d7880de1dfb672a0fe44774ac9e1353f5f
maonimisi <mabdulrasheed1210@gmail.com>
2023-06-08 14:20:28 -0700

    easy print

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 __import__("os").write(1, "#Pythoniscool\n".encode("utf-8"))
-=======
-exec("import builtins\nbuiltins.print('#pythoniscool')")
->>>>>>> 6eda08fadc0666bd74d858608b178c7741e6e245
