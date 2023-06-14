17975d8bf4d902467ecd5f8692120659dead4c52
maonimisi <mabdulrasheed1210@gmail.com>
2023-06-14 13:48:27 -0700

    weight average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list is None or len(my_list) == 0:
-        return None
+        return 0
     score_total = 0
     weight_total = 0
     for score, weight in my_list:
