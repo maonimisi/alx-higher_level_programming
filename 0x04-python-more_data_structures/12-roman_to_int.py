2bf0a5772a37cbff5a4dd0f78b706103d8c4255e
maonimisi <mabdulrasheed1210@gmail.com>
2023-06-14 09:52:47 -0700

    Roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
-import roman
-
-
 def roman_to_int(roman_string):
-    if roman_string is None or roman_string == '':
+    if roman_string is None or isinstance(roman_string, str) is False:
         return 0
-    number = roman.fromRoman(roman_string)
-    return number
+    res = 0
+    key = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+        }
+    r_length = len(roman_string)
+    for i in range(r_length):
+        if i < r_length - 1 and key[roman_string[i]] < \
+           key[roman_string[i + 1]]:
+            res -= key[roman_string[i]]
+        else:
+            res += key[roman_string[i]]
+    return res
