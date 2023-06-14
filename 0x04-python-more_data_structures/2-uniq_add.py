9374d330d3af4e4196b83df895711690dd89c05d
maonimisi <mabdulrasheed1210@gmail.com>
2023-06-14 05:22:43 -0700

    uniq add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    if my_list is None:
+        return None
     new_list = []
     for elem in range(len(my_list)):
         if my_list[elem] not in new_list:
