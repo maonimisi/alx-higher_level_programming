8ccda81cc8dbe25607fd21a0280a75c809679a22
maonimisi <mabdulrasheed1210@gmail.com>
2023-06-14 05:45:07 -0700

    common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    new_set_1 = []
-    new_set_2 = []
     if set_1 is None or set_2 is None:
         return None
+    common_elem = []
     for elem in set_1:
-        new_set_1.append(elem)
-    for elem in set_2:
-        new_set_2.append(elem)
-    for elem in range(len(new_set_1)):
-        if new_set_1[elem] in new_set_2:
-            return new_set_1[elem]
-        else:
-            continue
+        if elem in set_2:
+            common_elem.append(elem)
+    return common_elem
