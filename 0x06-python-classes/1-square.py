48d419238751e9c84c18ca37f31def20424db816
maonimisi <mabdulrasheed1210@gmail.com>
2023-06-27 07:49:36 -0700

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Documentation for a square class"""
+
+
+class Square:
+    """Represent a square class of quadrilateral with four equal sides
+
+    Attributes:
+        __size (int): size of a side of the square """
+
+
+    def __init__(self, size):
+        """ Initialize a square 
+        Args:
+            size(int): size of a side of the square
+        Return: None
+        """
+        self.__size = size
