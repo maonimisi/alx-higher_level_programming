c0b495d125888eeae19cee113b0aaeeb9a05999d
maonimisi <mabdulrasheed1210@gmail.com>
2023-07-11 05:09:29 -0700

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""write file function documentation"""
+
+
+def write_file(filename="", text=""):
+    """Args:
+	text(str) to write to the file
+       Return:
+	returns the number of text written
+    """
+
+    with open(filename, 'w', encoding='UTF-8') as myfile:
+        return myfile.write(text)
