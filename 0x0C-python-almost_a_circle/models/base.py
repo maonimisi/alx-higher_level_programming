0489268b872af0c35f54c24c231240f65ff60e8f
maonimisi <mabdulrasheed1210@gmail.com>
2023-07-16 11:47:14 -0700

    almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Base Class Documentation"""
+
+
+import json
+
+
+class Base:
+    """Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Instantiation function for base Instance
+
+        Args:
+       
+        id(int): the id of the instance
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
