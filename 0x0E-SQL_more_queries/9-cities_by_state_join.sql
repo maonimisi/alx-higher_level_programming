d27e94dd9fc1a7aba33fe2771c96572dfe53086f
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-08-16 22:24:23 +0100

    Cities by State Join

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- lists all cities contained in the database hbtn_0d_usa
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` as c
+        INNER JOIN `states` as s
+        ON c.`state_id` = s.`id`
+  ORDER BY c.`id`
