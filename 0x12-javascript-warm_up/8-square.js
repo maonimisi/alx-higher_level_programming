dc8d6eaf0e0a5fc63ec4d4b389c80c841c9c941c
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-09-11 14:44:48 +0100

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// script that prints a square
+
+// check if first argument can be converted to an integer
+const firstArg = Math.floor(Number(process.argv[2]));
+
+if (Number.isNaN(Number(firstArg))) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < firstArg; i++) {
+    console.log('x'.repeat(firstArg));
+  }
+}
