dcd7c203772a90136dbf771e6ad393d0a13756e6
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-09-11 14:20:17 +0100

    Multi language loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* script that prints 3 lines: (like 1-multi_languages.js)
+ but by using an array of string and a loop
+ */
+
+const arrayOfStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arrayOfStrings.length; i++) {
+  console.log(arrayOfStrings[i]);
+}
