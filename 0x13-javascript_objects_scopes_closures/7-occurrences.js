a06ba9fc4928e954e2fd0baf59ff5584b93b91eb
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-09-12 20:29:10 +0100

    Function that return the number of occurence in a list

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// function that returns the number of occurrences in a list
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const i in list) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
