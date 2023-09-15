3d1abbaf25d703f7f848469c627f7ec6ed872c5f
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-09-15 12:33:56 +0100

    script that lists all State objects that contain the letter a from the database

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+script that prints the first State object from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (user, password, database), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id.asc()).all()
+
+    for state in states:
+        if 'a' in state.name:
+            print("{}: {}".format(states.id, states.name))
+    session.close()
