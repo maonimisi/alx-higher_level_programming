3b814c84590cd86f01fdf137876bcc24352cae5b
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-09-28 12:10:16 +0100

    Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, 
-# And displays the size of the body of the response
-
-curl -s "$1" | wc -c
+# Bash script that takes in a URL, sends a request to that URL, And displays the size of the body of the response
+curl -so /dev/null "$1" -w '%{size_download}\n'
