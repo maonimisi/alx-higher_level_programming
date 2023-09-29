6b0434ccb15bb799377d3e0580e3834f1845b333
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-09-29 10:24:50 +0100

    Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+import requests
+
+
+if __name__ == "__main__":
+    request = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response")
+    print("\t- type: {}".format(type(request.text)))
+    print("\t- content: {}".format(request.text))
