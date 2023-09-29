eb1c7273d79ae2bc7bbfa17d2cbdd4745290a286
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-09-29 15:07:41 +0100

    Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    url = "http://0.0.0.0:5000/search_user"
+    payload = {"q": letter}
+    
+    response = requests.post(url, data=payload)
+    
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
