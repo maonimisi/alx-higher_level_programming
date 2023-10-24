e69ee28eb67950b05601fbc09087127692121fb4
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-10-24 11:53:48 +0100

    Script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that writes a string to a file
+
+const request = require('fs');
+const oldFileName = process.argv[2];
+const newFileName = process.argv[3];
+
+request.writeFile(oldFileName, newFileName, 'utf-8', function(error) {
+  if (error) {
+    console.log(error);
+    }
+});
