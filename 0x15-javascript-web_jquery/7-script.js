66037c5ad7ad2743c3cebf2427a1d3be3cdd66a2
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-11-02 05:52:53 +0100

    JavaScript script that fetches the character name from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,6 +1,6 @@
 // JavaScript script that fetches the character name from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json
 const $ = window.$;
-const url = https://swapi-api.alx-tools.com/api/people/5/?format=json
+const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
 
 $(function () {
   $.get(url, function (data, statusText) {
